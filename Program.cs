ca52908f7a51b9c5a5ee4ba7b3f1ff55448b9df4
unknown <7221798@gmail.com>
2022-05-17 08:26:54 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace git_credential_cert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("arguments:");
+            
+
+            string message = string.Join(",", args);
+            string caption = "Error Detected in Input";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            result = MessageBox.Show(message, caption, buttons);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # git-credential-cert
 Protect git repository auth token with certificate from hardware token
+
+```
+git config --global credential.helper cert
+```
